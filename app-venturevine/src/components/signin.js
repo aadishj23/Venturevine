43a6eeeb977922ad5113c5949f3ab5672473b0b3
Aadish Jain <aadishjain017@gmail.com>
2024-04-17 21:36:21 +0530

    sign in, signup functionality lodged to console, database addition left

--- app-venturevine/src/components/signin.js
+++ app-venturevine/src/components/signin.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import './signin.css'
 function Signin() {
+  const [signInData,setSignInData]=React.useState({Name: "", Password: "" })
+  function handleChangeSignIn(event){
+  const {name,value}=event.target
+    setSignInData(prevSignInData => {
+      return {
+        ...prevSignInData,
+        [name]:value
+      }
+    })
+  }
+  // console.log(signInData)
+  function handleSubmitSignIn(event){
+    event.preventDefault()
+    // Database request
+  }
   return (
     <div className='signin'>
-        <input type="text" placeholder='Enter Email/Phone' />
-        <input type="password" placeholder='Enter Password' />
-        <button>SignIn</button>
+      <form onSubmit={handleSubmitSignIn}>
+        <input 
+            type="text" 
+            placeholder='Enter Email/Phone' 
+            name="Name" 
+            value={signInData.Name} 
+            onChange={handleChangeSignIn}
+        />
+        <input 
+          type="password" 
+          placeholder='Enter Password' 
+          name="Password" 
+          value={signInData.Password} 
+          onChange={handleChangeSignIn}
+        />
+        <button type='submit'>SignIn</button>
+      </form>
     </div>
   )
 }
--- app-venturevine/src/components/signup.js
+++ app-venturevine/src/components/signup.js
@@ -2,18 +2,20 @@ import React from 'react'
 import './signup.css'
 function Signup() {
   const [signUpData,setSignUpData]=React.useState({Name: "" ,Email: "" , Password: "",ConfirmPassword: ""  })
-    function handleChange(event){
-      const {name,value}=event.target
-      setSignUpData(prevSignUpData => {
-        return {
-          ...prevSignUpData,
-          [name]:value
-        }
-      })
-    }
-    function handleSubmit(event){
-      event.preventDefault()
-    }
+  function handleChange(event){
+  const {name,value}=event.target
+    setSignUpData(prevSignUpData => {
+      return {
+        ...prevSignUpData,
+        [name]:value
+      }
+    })
+  }
+  // console.log(signUpData)
+  function handleSubmit(event){
+    event.preventDefault()
+    // Database request
+  }
   return (
     <div className='signup'>
       <form onSubmit={handleSubmit}>
@@ -24,10 +26,28 @@ function Signup() {
           value={signUpData.Name} 
           onChange={handleChange}
         />
-        <input type="text" placeholder='Enter Email' />
-        <input type="password" placeholder='Enter your password'/>
-        <input type="password" placeholder='Confirm password'/>
-        <button>SignUp</button>
+        <input 
+          type="text" 
+          placeholder='Enter Email' 
+          name="Email" 
+          value={signUpData.Email} 
+          onChange={handleChange}
+        />
+        <input 
+          type="password" 
+          placeholder='Enter your password'
+          name="Password" 
+          value={signUpData.Password} 
+          onChange={handleChange}
+        />
+        <input 
+          type="password" 
+          placeholder='Confirm password'
+          name="ConfirmPassword" 
+          value={signUpData.ConfirmPassword} 
+          onChange={handleChange}
+        />
+        <button type='submit'>SignUp</button>
       </form>
     </div>
   )
