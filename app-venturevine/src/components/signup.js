045a56cf8da708c9c5aa353516232677da0dfa36
Aadish Jain <aadishjain017@gmail.com>
2024-04-17 18:36:53 +0530

    added state to sign-up form

--- app-venturevine/src/components/signup.js
+++ app-venturevine/src/components/signup.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import './signup.css'
 function Signup() {
+  const [signUpData,setSignUpData]=React.useState({Name: "" ,Email: "" , Password: "",ConfirmPassword: ""  })
+    function handleChange(event){
+      const {name,value}=event.target
+      setSignUpData(prevSignUpData => {
+        return {
+          ...prevSignUpData,
+          [name]:value
+        }
+      })
+    }
+    function handleSubmit(event){
+      event.preventDefault()
+    }
   return (
     <div className='signup'>
-        <input type='text' placeholder='Enter your Name' />
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text' 
+          placeholder='Enter your Name' 
+          name="Name" 
+          value={signUpData.Name} 
+          onChange={handleChange}
+        />
         <input type="text" placeholder='Enter Email' />
         <input type="password" placeholder='Enter your password'/>
         <input type="password" placeholder='Confirm password'/>
         <button>SignUp</button>
+      </form>
     </div>
   )
 }
